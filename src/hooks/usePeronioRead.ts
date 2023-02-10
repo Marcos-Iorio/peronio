fa97bf185518152595ccf1ddb1df02d785e757a2
Agustin Kassis <webmaster@masize.com>
2023-02-09 23:28:41 -0300

    added usePeronioRead hook

---
+++ src/hooks/usePeronioRead.ts
@@ -0,0 +1,18 @@
+import { useContractRead } from "wagmi";
+import usePeronio from "./usePeronio";
+
+/**
+ * Hook for reading from Peronio contract
+ * @param method
+ * @param args
+ * @example https://wagmi.sh/react/hooks/useContractRead
+ */
+export const usePeronioRead = (method: string, args = []) => {
+  const contract = usePeronio();
+
+  return useContractRead(contract, method, {
+    args,
+  });
+};
+
+export default usePeronioRead;
