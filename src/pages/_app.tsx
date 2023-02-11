04cf9e771602c729f7e7d48f68b26220aa220c3c
Agustin Kassis <webmaster@masize.com>
2023-02-11 16:01:16 -0300

    tidy _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,36 +2,24 @@ import { AppProps } from "next/app";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar/Navbar";
 
-import {createClient, WagmiConfig, configureChains } from "wagmi";
-import {polygon} from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
-
-import { ConnectKitProvider, getDefaultClient  } from "connectkit";
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-
-const { chains, provider } = configureChains(
-  [
-    polygon
-  ],
-  [publicProvider()]
-);
-
+import { WagmiConfig, createClient } from "wagmi";
+import { polygon } from "wagmi/chains";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
 
 const client = createClient(
   getDefaultClient({
     appName: "Peronio dApp",
-    provider,
-    chains
-  }),
+    //infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
+    //alchemyId:  process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    chains: [polygon],
+  })
 );
 
-
-
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
     <WagmiConfig client={client}>
-      <ConnectKitProvider  options={{
+      <ConnectKitProvider
+        options={{
           language: "es-ES",
           hideNoWalletCTA: true,
           embedGoogleFonts: true,
@@ -45,12 +33,12 @@ function MyApp({ Component, pageProps }: AppProps) {
           "--ck-body-background": "#1B262C",
           "--ck-body-background-transparent": "#1B262C",
           "--ck-primary-button-background": "#00B7C2",
-        }}>
+        }}
+      >
         <Navbar />
         <Component {...pageProps} />
       </ConnectKitProvider>
     </WagmiConfig>
-      
   );
 }
 
