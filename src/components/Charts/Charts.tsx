0eefcc9b1f15ebc1ad9990e77595b60a5f291c7f
Marcos Iorio <marcossiorio@gmail.com>
2023-02-13 13:54:34 -0300

    finishing charts

--- src/components/Charts/Charts.tsx
+++ src/components/Charts/Charts.tsx
@@ -8,6 +8,9 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
+import GaugeChart from 'react-gauge-chart'
+import Image from "next/image";
+
 import usePeronioRead from "../../hooks/usePeronioRead";
 
 const dataEx = [
@@ -56,8 +59,6 @@ const dataEx = [
 ];
 
 const Charts = () => {
-  const {data, isError, isLoading} = usePeronioRead("collateralRatio", [])
-
   return (
     <>
       <div className="flex flex-row gap-5 min-w-full max-w-max">
@@ -88,8 +89,8 @@ const Charts = () => {
             >
               <defs>
                 <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.3}/>
+                  <stop offset="80%" stopColor="#82ca9d" stopOpacity={0}/>
               </linearGradient>
               </defs>
               <XAxis dataKey="name" />
@@ -103,20 +104,113 @@ const Charts = () => {
             </AreaChart>
           </ResponsiveContainer>
         </div>
-
         <div className="p-5 xl:basis-2/6 2xl:basis-1/5 min-w-max border-solid border-2 border-[#00B7C2] bg-[#0B4D76]/50 rounded-md backdrop-blur-sm">
-          s
+          <div className="flex flex-col items-center justify-center w-full h-full">
+            <div className="bg-[#185e86]/30 w-full p-5 rounded-md">
+              <p className="text-lg font-Roboto text-center">USDT/PE</p>
+              <p className="text-center text-6xl">240</p>
+            </div>
+            <div className="w-full h-[1px] my-2 bg-[#82ca9d]"></div>
+            <div className="bg-[#185e86]/30 w-full p-5 rounded-md">
+              <p className="text-lg font-Roboto text-center">Dólar Blue.</p>
+              <p className="text-center text-6xl">380</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex flex-row gap-5 min-w-full flex-nowrap flex-grow-0-1">
-        <div className="p-5 xl:basis-2/6 2xl:basis-1/5 w-full border-solid border-2 border-[#00B7C2] bg-[#0B4D76]/50 backdrop-blur-sm rounded-md h-full min-h-[20em]">
-          asda
+      <div className="flex flex-row gap-5 min-w-full flex-nowrap flex-grow-0-1 max-h-[20rem]">
+        <div className="p-5 xl:basis-[15%] 2xl:basis-[20%] w-full border-solid border-2 border-[#00B7C2] bg-[#0B4D76]/50 backdrop-blur-sm rounded-md h-full min-h-[20em]">
+            <div className="flex flex-col h-full w-full items-center justify-center align-middle">
+              <div className="flex flex-row w-full justify-between">
+                  <p className="text-2xl font-Roboto">Equilibrado</p>
+                  <span className="text-lg font-Roboto rounded-full bg-[#00B7C2] w-7 h-7 text-center">?</span>
+              </div>
+              <div className="flex flex-col justify-center items-center py-5">
+                <p className="text-4xl font-Roboto">0.0041</p>
+                <p className="text-2xl font-Roboto">+3%</p>
+              </div>
+              <div className="mt-auto h-full relative">
+                <p className="absolute left-16 top-0 font-Roboto">0%</p>
+                <p className="absolute right-16 top-0 font-Roboto">+5%</p>
+                <GaugeChart id="gauge-chart4" 
+                  colors={["#ff4e4e", "#ffe851", "#61ff5e"]}
+                  nrOfLevels={3} 
+                  arcPadding={0.1} 
+                  cornerRadius={3}
+                  needleColor="#fff"
+                  needleBaseColor="#fff"
+                  percent={0.6}
+                  hideText={true}
+                />
+              </div>
+            </div>
         </div>
-        <div className="p-5 xl:basis-2/6 2xl:basis-2/5 min-w-max border-solid border-2 border-[#00B7C2] bg-[#0B4D76]/50 rounded-md backdrop-blur-sm ">
-          s
+        <div className="p-5 xl:basis-[37.6%] 2xl:basis-2/5 border-solid border-2 border-[#00B7C2] bg-[#0B4D76]/50 backdrop-blur-sm rounded-md xl:min-h-[20rem] 2xl:min-h-[25rem] xl:max-w-[80%] flex flex-col">
+          <div className="flex flex-row w-full justify-center">
+            <p className="text-2xl font-Roboto">0.0041</p>
+            <div className="font-Roboto xl:text-xl basis-2/4 2xl:text-2xl font-bold flex flex-col items-center">
+              <p>Liquidity pool</p>
+              <span>(PE/USDT)</span>
+            </div>
+            <span className="text-lg font-Roboto rounded-full bg-[#00B7C2] w-7 h-7 text-center ml-auto">?</span>
+          </div>
+          <ResponsiveContainer className="xl:h-[15rem] 2xl:h-[20rem] w-full">
+            <AreaChart
+              width={300}
+              height={200}
+              data={dataEx}
+              margin={{
+                top: 40,
+                right: 0,
+                left: 0,
+                bottom: 40,
+              }}
+            >
+              <defs>
+                <linearGradient id="lineColor" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#DD0FAA" stopOpacity={0.3}/>
+                  <stop offset="80%" stopColor="#DD0FAA" stopOpacity={0}/>
+              </linearGradient>
+              </defs>
+              <XAxis dataKey="name" />
+              <Area
+                type="linear"
+                dataKey="uv"
+                fillOpacity={1}
+                stroke="#DD0FAA"
+                fill="url(#lineColor)"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
         </div>
-        <div className="p-5 xl:basis-2/6 2xl:basis-2/5 min-w-max border-solid border-2 border-[#00B7C2] bg-[#0B4D76]/50 rounded-md backdrop-blur-sm ">
-          s
+        <div className="p-3 xl:basis-2/6 2xl:basis-2/5 min-w-max border-solid border-2 border-[#00B7C2] bg-[#0B4D76]/50 rounded-md backdrop-blur-sm ">
+          <p className="2xl:hidden font-Roboto text-2xl text-center h-fit">Bóveda</p>
+          <div className="flex 2xl:flex-row xl:flex-col justify-center w-full">
+            <div className="xl:hidden flex flex-col w-full justify-center items-center">
+              <p>Bóveda</p>
+              <Image src="./public/boveda.svg" alt="Ilustración de bóveda" width={100} height={100}/>
+            </div>
+            <div className="flex flex-col gap-2 py-2 px-5 w-full h-fit">
+              <div className="flex flex-col w-full">
+                <p className=" font-Roboto text-md">Respaldo en USDT</p>
+                <div className="border-2 border-solid border-[#00B7C2] bg-[#0D0D0D]/50 rounded-md p-2 w-full">
+                  <p className="text-center font-Roboto xl:text-2xl 2xl:text-3xl">10.000</p>
+                </div>
+              </div>
+              <div className="flex flex-col w-full">
+                <p className=" font-Roboto text-md">Respaldo en USDT</p>
+                <div className="border-2 border-solid border-[#00B7C2] bg-[#0D0D0D]/50 rounded-md p-2 w-full">
+                  <p className="text-center font-Roboto xl:text-2xl 2xl:text-3xl">10.000</p>
+                </div>
+              </div>
+              <div className="flex flex-col w-full">
+                <p className=" font-Roboto text-md">Respaldo en USDT</p>
+                <div className="border-2 border-solid border-[#00B7C2] bg-[#0D0D0D]/50 rounded-md p-2 w-full">
+                  <p className="text-center font-Roboto xl:text-2xl 2xl:text-3xl">10.000</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
