394c115b094c8579541c26c169c11e3b4a0f85de
Marcos Iorio <marcossiorio@gmail.com>
2023-02-24 15:04:50 -0300

    updated footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,11 +7,25 @@ import logo from "/public/logo-white.png";
 
 const Footer = () => {
     return(
-        <footer className="flex flex-col gap-3 w-full h-full pb-10">
+        <footer className="flex flex-col gap-3 w-full h-full">
             <div className="bg-[#00B7C2] 2xl:w-[95%] laptop:mx-auto 2xl:h-px laptop:w-[93%] laptop:h-px md:w-[93%] mobile:w-[93%] mobile:mx-3 mobile:h-px md:h-px"></div>
-            <div className="flex laptop:flex-row mobile:flex-col gap-5 w-full laptop:justify-between py-3 px-16 h-full">
-                <div>
+            <div className="flex laptop:flex-row mobile:flex-col gap-5 w-full laptop:justify-evenly py-10 px-16 h-full">
+                <div className="flex flex-col gap-3">
                     <Image src={logo} height={150} width={150} alt="Logo Peronio"/>
+                    <div className="flex flex-row gap-4 py-3">
+                        <Link href="">
+                            <Icon.BsTwitter className="w-5 h-auto text-[#00B7C2]"/>
+                        </Link>
+                        <Link href="">
+                            <Icon.BsTelegram className="w-5 h-auto text-[#00B7C2]"/>
+                        </Link>
+                        <Link href="">
+                            <Icon.BsInstagram className="w-5 h-auto text-[#00B7C2]"/>
+                        </Link>
+                        <Link href="">
+                            <Icon.BsGithub className="w-5 h-auto text-[#00B7C2]"/>
+                        </Link>
+                    </div>
                 </div>
                 <div className="flex flex-col gap-3">
                     <p className="font-Roboto font-bold text-xl text-[#00B7C2]">Desarrolladores</p>
@@ -28,20 +42,7 @@ const Footer = () => {
                     <Link href={'hhttps://docs.google.com/forms/d/e/1FAIpQLScCugMNil6Y4SCqUthOuchA6sAN1vCKBRFFL2ieVviYBRleBg/viewform'} target="_blank" rel="noreferrer" className="font-Roboto text-white text-lg hover:underline">Contacto</Link>
                 </div>
             </div>
-            <div className="flex flex-row gap-4 py-3 px-16">
-                <Link href="">
-                    <Icon.BsTwitter className="w-5 h-auto text-[#00B7C2]"/>
-                </Link>
-                <Link href="">
-                    <Icon.BsTelegram className="w-5 h-auto text-[#00B7C2]"/>
-                </Link>
-                <Link href="">
-                    <Icon.BsInstagram className="w-5 h-auto text-[#00B7C2]"/>
-                </Link>
-                <Link href="">
-                    <Icon.BsGithub className="w-5 h-auto text-[#00B7C2]"/>
-                </Link>
-            </div>
+            
         </footer>
     );
 }
