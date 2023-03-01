1a7fb68ee75949da3852de0b23d7747f0d112272
Marcos Iorio <marcossiorio@gmail.com>
2023-03-01 20:30:56 -0300

    fixed responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,11 +11,14 @@ const Navbar = () => {
   return (
     <nav className="2xl:flex 2xl:flex-col laptop:flex laptop:flex-col lg:flex-col md:flex-col md:flex w-full items-center z-50">
       <div className="2xl:flex 2xl:flex-row 2xl:w-full 2xl:py-5 2xl:px-12 2xl:items-center laptop:flex laptop:flex-row lg:flex-row md:flex md:flex-row md:py-5 md:px-7 md:w-full laptop:w-full laptop:py-5 laptop:px-12 laptop:items-center">
-        <Link href="/" className="basis-2/12">
+        <Link
+          href="/"
+          className="xl:basis-2/12 laptop:basis-[10%] md:flex md:items-center"
+        >
           <Image src={logo} alt="Logo Peronio" width={150} height={150}></Image>
         </Link>
-        <div className="relative md:basis-1/2 md:ml-7">
-          <ul className="2xl:basis-1/2 2xl:flex 2xl:flex-row 2xl:gap-8 2xl:items-center laptop:basis-1/2 laptop:flex laptop:flex-row laptop:gap-5 laptop:items-center md:basis-2/3 md:flex md:flex-row md:gap-10 md:items-center">
+        <div className="relative md:basis-full xl:basis-1/2 md:ml-7">
+          <ul className="2xl:basis-1/2 2xl:flex 2xl:flex-row 2xl:gap-8 2xl:items-center laptop:basis-1/2 laptop:flex h-full laptop:flex-row laptop:gap-2 laptop:items-center md:basis-2/3 md:flex md:flex-row md:gap-4 md:items-center">
             {menu.map((item, index) => (
               <NavElement key={index} menuItem={item} />
             ))}
--- src/components/WizardButton/WizardButton.tsx
+++ src/components/WizardButton/WizardButton.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { WizardContext } from "../../contexts/WizardContext";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
 const liVariant = {
   opened: {
@@ -28,7 +28,7 @@ const WizardButton = () => {
     <motion.button
       onClick={openModalHandler}
       variants={liVariant}
-      className="border-[#00B7C2] border-solid border-2 rounded-xl text-Roboto xl:text-sm font-normal xl:py-2 xl:px-5 xl:mt-0 text-white shadow-wizard-button mobile:mt-10 mobile:font-Abril mobile:text-2xl mobile:py-5"
+      className="border-[#00B7C2] border-solid border-2 rounded-xl text-Roboto xl:text-md font-normal xl:py-2 xl:px-10 xl:w-fit xl:mt-0 text-white shadow-wizard-button md:text-sm md:w-[24%] md:px-0 md:mt-0 md:py-2 mobile:mt-10 mobile:font-Abril mobile:text-2xl mobile:py-5"
     >
       ¿Cómo comenzar?
     </motion.button>
