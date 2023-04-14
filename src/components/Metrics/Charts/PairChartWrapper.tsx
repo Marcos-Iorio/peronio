4b4f89992a7f0830a0744de24a7462201570e0e7
Marcos Iorio <marcossiorio@gmail.com>
2023-04-14 19:51:16 -0300

    added loading charts

--- src/components/Metrics/Charts/PairChartWrapper.tsx
+++ src/components/Metrics/Charts/PairChartWrapper.tsx
@@ -53,6 +53,7 @@ const PairChartWrapper = ({ arsPrice }: IChartWrapper) => {
   const valueToDisplay = hoverValue?.toFixed(3) || arsPricePerPe;
 
   useEffect(() => {
+    setIsLoading(true);
     const data = getPairPrices("DAY", historicArsPrices, false).then(
       (response) => {
         setArsPriceData(response);
@@ -111,75 +112,76 @@ const PairChartWrapper = ({ arsPrice }: IChartWrapper) => {
     setArsPriceData(data);
     setTimeWindow(time);
   };
-  if (isLoading) {
-    return <LoadingChart />;
-  }
-  return (
-    <>
-      <div className="flex mobile:flex-col xl:flex-row w-full justify-between">
-        <div className="flex flex-row gap-3">
-          <div className="flex flex-col">
-            <div className="font-Roboto xl:text-3xl mobile:text-3xl font-bold">
-              {valueToDisplay}
+  if (!isLoading) {
+    return (
+      <>
+        <div className="flex mobile:flex-col xl:flex-row w-full justify-between">
+          <div className="flex flex-row gap-3">
+            <div className="flex flex-col">
+              <div className="font-Roboto xl:text-3xl mobile:text-3xl font-bold">
+                {valueToDisplay}
+              </div>
+              <div>{hoverDate || currentDate}</div>
             </div>
-            <div>{hoverDate || currentDate}</div>
+            <div
+              style={{ color: isChangePositive ? "#31D0AA" : "#ED4B9E" }}
+              className="font-Roboto text-lg mobile:text-xl"
+            >{`${isChangePositive ? "+" : ""}${changeValue.toFixed(
+              3
+            )} (${changePercentage}%)`}</div>
           </div>
-          <div
-            style={{ color: isChangePositive ? "#31D0AA" : "#ED4B9E" }}
-            className="font-Roboto text-lg mobile:text-xl"
-          >{`${isChangePositive ? "+" : ""}${changeValue.toFixed(
-            3
-          )} (${changePercentage}%)`}</div>
+          <div className="font-Roboto xl:text-2xl font-bold flex flex-row mobile:justify-between mobile:mb-2 gap-5">
+            <p className="xL:text-2xl mobile:text-xl font-Roboto">
+              Precio PE/ARS
+            </p>
+            <InfoPopover
+              title='¿Que significa "Precio PE/ARS"?'
+              text="El gráfico muestra información sobre el precio frente al Peso Argetino en tres períodos diferentes de tiempo."
+            />
+          </div>
+          <div className="flex flex-row mobile:justify-evenly laptop:justify-start laptop:w-fit rounded-md border-solid border-[#00B7C2] border bg-[#363636]/50 backdrop-blur-md gap-1 p-2">
+            <button
+              onClick={() => changeTimeWindowHandler(0)}
+              style={{
+                backgroundColor: timeWindow === 0 ? "#00B7C2" : "#1b1b1b"
+              }}
+              className="font-Abril text-lg font-normal  bg-[#1b1b1b]/30 hover:bg-[#3b3b3b] py-1 px-3 rounded-md"
+            >
+              24H
+            </button>
+            <button
+              onClick={() => changeTimeWindowHandler(1)}
+              style={{
+                backgroundColor: timeWindow === 1 ? "#00B7C2" : "#1b1b1b"
+              }}
+              className="font-Abril text-lg font-normal bg-[#1b1b1b]/30 hover:bg-[#3b3b3b] py-1 px-3 rounded-md"
+            >
+              1W
+            </button>
+            <button
+              onClick={() => changeTimeWindowHandler(2)}
+              style={{
+                backgroundColor: timeWindow === 2 ? "#00B7C2" : "#1b1b1b"
+              }}
+              className="font-Abril text-lg font-normal  bg-[#1b1b1b]/30 hover:bg-[#3b3b3b] py-1 px-3 rounded-md"
+            >
+              1M
+            </button>
+          </div>
         </div>
-        <div className="font-Roboto xl:text-2xl font-bold flex flex-row mobile:justify-between mobile:mb-2 gap-5">
-          <p className="xL:text-2xl mobile:text-xl font-Roboto">
-            Precio PE/ARS
-          </p>
-          <InfoPopover
-            title='¿Que significa "Precio PE/ARS"?'
-            text="El gráfico muestra información sobre el precio frente al Peso Argetino en tres períodos diferentes de tiempo."
-          />
-        </div>
-        <div className="flex flex-row mobile:justify-evenly laptop:justify-start laptop:w-fit rounded-md border-solid border-[#00B7C2] border bg-[#363636]/50 backdrop-blur-md gap-1 p-2">
-          <button
-            onClick={() => changeTimeWindowHandler(0)}
-            style={{
-              backgroundColor: timeWindow === 0 ? "#00B7C2" : "#1b1b1b"
-            }}
-            className="font-Abril text-lg font-normal  bg-[#1b1b1b]/30 hover:bg-[#3b3b3b] py-1 px-3 rounded-md"
-          >
-            24H
-          </button>
-          <button
-            onClick={() => changeTimeWindowHandler(1)}
-            style={{
-              backgroundColor: timeWindow === 1 ? "#00B7C2" : "#1b1b1b"
-            }}
-            className="font-Abril text-lg font-normal bg-[#1b1b1b]/30 hover:bg-[#3b3b3b] py-1 px-3 rounded-md"
-          >
-            1W
-          </button>
-          <button
-            onClick={() => changeTimeWindowHandler(2)}
-            style={{
-              backgroundColor: timeWindow === 2 ? "#00B7C2" : "#1b1b1b"
-            }}
-            className="font-Abril text-lg font-normal  bg-[#1b1b1b]/30 hover:bg-[#3b3b3b] py-1 px-3 rounded-md"
-          >
-            1M
-          </button>
-        </div>
-      </div>
-      <ArsPriceChart
-        setHoverDate={setHoverDate}
-        setHoverValue={setHoverValue}
-        data={pairPriceData}
-        colors={colors}
-        gradientId="arsColor"
-        timeWindow={timeWindow}
-      />
-    </>
-  );
+        <ArsPriceChart
+          setHoverDate={setHoverDate}
+          setHoverValue={setHoverValue}
+          data={pairPriceData}
+          colors={colors}
+          gradientId="arsColor"
+          timeWindow={timeWindow}
+        />
+      </>
+    );
+  }
+
+  return <LoadingChart />;
 };
 
 export default PairChartWrapper;
